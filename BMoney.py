c2254f3975c9174335e1e40f80ecce345f3947df
Uriegas <1930526@upv.edu.mx>
2021-03-04 20:15:29 -0600

    Main

--- BMoney.py
+++ BMoney.py
@@ -1,10 +1,24 @@
 class BMoney:
-    def __init__(self):
-        self.money
+    def __init__(self, money = 0.0):
+        self.money = money
     def madd(self, m1, m2):
-        self.money = m1 + m2
+        self.putMoney( m1 + m2 )
 
     def getMoney(self):
         return self.money
+
     def putMoney(self, money):
         self.money = money
+
+    def __add__(self, m2):
+        return self.money + m2.getMoney()
+
+
+if __name__ == '__main__':
+    dolares = BMoney()
+    d1 = BMoney(10.1)
+    d2 = BMoney(10)
+    print(dolares.getMoney())
+    dolares.madd(d1, d2)
+    print(dolares.getMoney())
+    
