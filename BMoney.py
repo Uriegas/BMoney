b3f4cf9b8798e2717360ee142ac459183570238c
Uriegas <1930526@upv.edu.mx>
2021-03-04 19:52:34 -0600

    Initial Commit

---
+++ BMoney.py
@@ -0,0 +1,10 @@
+class BMoney:
+    def __init__(self):
+        self.money
+    def madd(self, m1, m2):
+        self.money = m1 + m2
+
+    def getMoney(self):
+        return self.money
+    def putMoney(self, money):
+        self.money = money
